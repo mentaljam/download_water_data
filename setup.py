6edd5d3e0accee98c87d1663c59d91cfb4b63953
Petr Tsymbarovich <petr@tsymbarovich.ru>
2023-07-12 09:34:09 +0300

    Switch to pathlib in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils import cmd
 import zipapp
-import os.path
+from pathlib import Path
 from setuptools import setup
 from download_water_data.__main__ import __version__
 
-SRC_DIR = os.path.abspath(os.path.dirname(__file__))
+SRC_DIR = Path(__file__).parent
 
 class PyzCommand(cmd.Command):
     '''A custom command to generate download_water_data.pyz.'''
@@ -22,12 +22,14 @@ class PyzCommand(cmd.Command):
 
     def run(self):
         '''Run command.'''
-        out_dir = os.path.join(SRC_DIR, 'dist')
-        if not os.path.isdir(out_dir):
-            os.makedirs(out_dir)
-        with open(os.path.join(SRC_DIR, 'download_water_data', '__main__.py'), 'rb') as main:
-            zipapp.create_archive(main, os.path.join(out_dir, 'download_water_data.pyz'),
-                                  interpreter='/usr/bin/env python')
+        out_dir = SRC_DIR / 'dist'
+        out_dir.mkdir(exist_ok=True)
+        with SRC_DIR.joinpath('download_water_data', '__main__.py').open('rb') as main:
+            zipapp.create_archive(
+                source=main,
+                target=out_dir / 'download_water_data.pyz',
+                interpreter='/usr/bin/env python',
+            )
 
 setup(
     cmdclass={
@@ -36,7 +38,7 @@ setup(
     name='download_water_data',
     version=__version__,
     description='Downloader for the Global Surface Water Data of the Copernicus Programme',
-    long_description=open(os.path.join(SRC_DIR, 'README.rst'), 'r').read(),
+    long_description=SRC_DIR.joinpath('README.rst').read_text(),
     url='https://github.com/mentaljam/download_water_data',
     author='Petr Tsymbarovich',
     author_email='petr@tsymbarovich.ru',
